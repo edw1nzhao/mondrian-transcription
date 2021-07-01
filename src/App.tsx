ed0def87b9662216acebcd06fec6d81c7fc5ed8c
ezhao7 <edwin.zhao@outlook.com>
2021-06-30 21:47:47 -0700

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import Sketch from "./Sketch"
+import './App.css';
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route,
+//   Link
+// } from "react-router-dom";
+import SideNav from './components/SideNav';
+
+export default function App() {
+  return (
+  // <Router>
+  //   <div>
+  //     {/* <nav>
+  //       <ul>
+  //         <li>
+  //           <Link to="/">Home</Link>
+  //         </li>
+  //         <li>
+  //           <Link to="/about">About</Link>
+  //         </li>
+  //         <li>
+  //           <Link to="/users">Users</Link>
+  //         </li>
+  //       </ul>
+  //     </nav> */}
+  //     <SideNav></SideNav>
+
+  //     {/* A <Switch> looks through its children <Route>s and
+  //         renders the first one that matches the current URL. */}
+  //     <Switch>
+  //       <Route path="/about">
+  //         {/* <About /> */}
+  //       </Route>
+  //       <Route path="/users">
+  //         {/* <Users /> */}
+  //       </Route>
+  //       <Route path="/">
+  //         {/* <Home /> */}
+  //       </Route>
+  //     </Switch>
+  //   </div>
+  // </Router>
+    <div>
+      <SideNav></SideNav>
+
+      <div className="App">
+        <Sketch />
+      </div>
+    </div>
+
+  );
+}
